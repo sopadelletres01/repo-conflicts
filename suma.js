0bcf5a5879b576e333314d2f3d54d8f50d1dc56f
sopadelletres01 <96629102+sopadelletres01@users.noreply.github.com>
2022-07-01 09:26:04 +0200

    Create suma.js

---
+++ suma.js
@@ -0,0 +1,18 @@
+function suma(n1,n2){
+  return n1 + n2
+}
+
+function resta(n1,n2){
+  return n1 - n2
+}
+
+function multiplicacion(n1,n2){
+  return n1 * n2
+}
+
+function division(n1,n2){
+  return n1 / n2
+}
+
+
+console.log(suma(2,4))
