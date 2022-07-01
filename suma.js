6808d0989d8f3857f0adcc4ee4628a45d53121d0
cristina <cristinaaran89@gmail.com>
2022-07-01 09:50:06 +0200

    actualizado

--- suma.js
+++ suma.js
@@ -10,9 +10,17 @@ function multiplicacion(n1,n2){
   return n1 * n2
 }
 
+// los que trabajen con CONSOLA BORRARA esta funcion
 function division(n1,n2){
   return n1 / n2
 }
 
 
-console.log(suma(2,4))
+console.log(suma(2, 4))
+
+
+//pasale a esta funcion lo que tu quieras
+console.log(resta())
+
+console.log(multiplicacion());
+console.log(division())
