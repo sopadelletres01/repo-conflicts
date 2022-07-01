69861951bc042bfe26b05ed0403586fcfa9d3569
christian.gecchelin <christin.gecchelin@gmail.com>
2022-07-01 10:17:45 +0200

    html

---
+++ .history/text_20220701101554.js
@@ -0,0 +1,35 @@
+
+const textList=[]
+
+      const addText=(letter)=>      {
+
+        textList.push(letter)
+
+
+
+              }
+
+
+          // Descomenta esta funcion Grupo 1
+
+          // const countLength=()=>{
+          //   const len=textList.length
+          //   console.log(len)
+          // }
+
+
+      
+      const print=()=>{
+  //modifica  la linea 18, pon lo que te apetezca SIN BORRARLA
+  
+  
+  
+      console.log("esta este es el contenido", textList)
+}
+
+// pasale a la funcion addText lo que tu quieras y copia la linea 31 tantas veces como quieras pasandole algo
+console.log(addText())
+
+console.log(print())
+
+console.log(countLength())
